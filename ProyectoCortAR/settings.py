337ba1df1a791f6fb105b129ab2a56c4521c11e1
Matias Cordero <corderomatias2013@gmail.com>
2023-08-20 05:31:41 -0300

    fsdf

--- ProyectoCortAR/settings.py
+++ ProyectoCortAR/settings.py
@@ -84,14 +84,15 @@ WSGI_APPLICATION = 'ProyectoCortAR.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+DATABASES['default'] = dj_database_url.parse("postgres://cortar_user:GXZy9uJ7FYnf9OhfZhJ1Lq0KNzpciIEt@dpg-cjgsqps1ja0c73chvsbg-a.oregon-postgres.render.com/cortar")
 
+# postgres://cortar_user:GXZy9uJ7FYnf9OhfZhJ1Lq0KNzpciIEt@dpg-cjgsqps1ja0c73chvsbg-a/cortar
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
